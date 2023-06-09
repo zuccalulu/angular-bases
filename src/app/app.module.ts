29a99837afe4ac1742a383ffad27ddf835a568c7
Luciana Zuccarelli <lzuccarelli@gmail.com>
2023-06-09 11:51:31 -0300

    Fin de sección 4

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CounterModule } from './counter/components/counter/counter.module';
+import { HeroesModule } from './heroes/components/heroes/heroes.module';
+import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+
+ // nombre de la clase del archivo .ts
+
+  ],
+  imports: [
+    BrowserModule,
+    CounterModule,
+    HeroesModule,
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
